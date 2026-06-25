b02282bf5b3174329e88383458f15d3d4d83072e
Sean Shahkarami <sean@Seans-MacBook.local>
2026-06-25 12:58:08 -0500

    ignore case

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def get_image_data_rtsp(image_uri: str) -> bytes:
     except subprocess.CalledProcessError as e:
         last_line = e.stderr.splitlines()[-1]
         # Raise a more informative exception, if ffmpeg provides its own error.
-        if "Error" in last_line:
+        if "error" in last_line.lower():
             raise RuntimeError(f"Error during RTSP fetch: {last_line}")
         # Fallback to reraising original exception.
         raise
